bed8285067b7c2562f06e8d4fd03e3ee8477adb8
Gabriel Campos <gaeducampos@gmail.com>
2024-10-05 08:21:57 -0600

    Initial Commit

---
+++ DigitalBuddy/DigitalBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalBuddyApp.swift
+//  DigitalBuddy
+//
+//  Created by Gabriel Campos on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DigitalBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
